c6798d1a1497c5cd1e7ae4cfc2888fd7b5030ae2
Merniez10 <merniez10@outlook.com>
2023-12-02 21:20:19 -0700

    finished prompts and testing cli

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-const inquirer = require('inquirer');
-const {Circle, Triangle, Square} = require('./lib/shapes.js');
-const fs = require('fs');
+const CLIprompt = require("./lib/cli")
 
-
-const questions = [
-    {
-        type: 'input',
-        name: 'logoText',
-        message: 'Choose three letters for your logo'
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'What color do you want for your text?'
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Which shape would you like to use for your logo?',
-        choices: ['Circle', 'Triangle', 'square']
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'What color do you want your shape to be?'
-    }
-
-];
-
+new CLIprompt().run();
